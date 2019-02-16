8b74503733d028330b44674014224e37d43e5a9f
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-02-16 21:58:24 +0000

    create meal service

---
+++ API/utils/dummyData.js
@@ -0,0 +1,31 @@
+export default {
+    meals: [
+        {
+            id: 1,
+            name: 'Fried Rice',
+            size: 'Medium',
+            price: '450'
+        },
+        
+        {
+            id: 2,
+            name: 'Jollof Rice',
+            size: 'Large',
+            price: '550'
+        },
+        
+        {
+            id: 3,
+            name: 'Beans',
+            size: 'Small',
+            price: '150'
+        },
+        
+        {
+            id: 4,
+            name: 'Eba & Semo',
+            size: 'Medium',
+            price: '300'
+        }
+    ]
+}
