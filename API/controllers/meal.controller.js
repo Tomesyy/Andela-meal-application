10e2e55685e49003d9e616d44036dd20de1f5920
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-02-16 22:16:31 +0000

    create meal controller

---
+++ API/controllers/meal.controller.js
@@ -0,0 +1,43 @@
+import mealService from '../services/meal.service'
+
+
+
+const mealController = {
+    fetchAllMeals(req, res){
+        const allMeals = mealService.fetchAllMeals();
+        return res.json({
+            status: 'sucess',
+            data: allMeals
+        }).status(200);
+    },
+    addMeal(req, res){
+        /*
+           Expect json of the format
+
+           {
+               name: 'food',
+               size: 'Large',
+               price: 900
+           }
+        */
+
+        const newMeal = req.body;
+        const createdMeal = mealService.addMeal(newMeal);
+        return res.json({
+            status: 'success',
+            data: createdMeal
+        }).status(201);
+    },
+    getSingleMeal(req, res){
+        const id = req.params.id;
+        const foundMeal = mealService.getMeal(id);
+        return res.json({
+            status: 'sucess',
+            data: allMeals
+        }).status(201);
+
+    }
+
+}
+
+export default mealController;
