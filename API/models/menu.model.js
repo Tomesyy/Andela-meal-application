85ad54407d7dc1b6b7f54db7a03abfac5f390965
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-02-17 09:52:29 +0000

    create menu model

---
+++ API/models/menu.model.js
@@ -0,0 +1,11 @@
+class Menu {
+    constructor(){
+        this.id = null;
+        this.name = null;
+        this.details = null;
+        this.price = null;
+        this.delivery = null;
+    }
+}
+
+export default Menu;
